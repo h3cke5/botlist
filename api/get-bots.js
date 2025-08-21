62734ea98d4be55fbe0abe34ba9ff7169566d811
h3cke5 <101654314+h3cke5@users.noreply.github.com>
2025-08-21 16:59:34 +0100

    Update get-bots.js

--- api/get-bots.js
+++ api/get-bots.js
@@ -1,4 +1,4 @@
-import { connectToDatabase } from "../../lib/db";
+import { connectToDatabase } from "../lib/db";
 
 export default async function handler(req, res) {
   const { userId } = req.query;
