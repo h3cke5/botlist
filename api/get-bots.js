3d93ae0e79e5d7e4b93bb4590f5df6f09d935eca
h3cke5 <101654314+h3cke5@users.noreply.github.com>
2025-08-21 16:33:10 +0100

    Create get-bots.js

---
+++ api/get-bots.js
@@ -0,0 +1,11 @@
+import { connectToDatabase } from "../../lib/db";
+
+export default async function handler(req, res) {
+  const { userId } = req.query;
+  if (!userId) return res.status(400).json({ error: "UserId necessário" });
+
+  const db = await connectToDatabase();
+  const bots = await db.collection("bots").find({ userId }).toArray();
+
+  res.status(200).json(bots);
+}
