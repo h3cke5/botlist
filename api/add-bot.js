156ea7a0256845159990d73446f2456173590fe1
h3cke5 <101654314+h3cke5@users.noreply.github.com>
2025-08-21 16:44:24 +0100

    Update add-bot.js

--- api/add-bot.js
+++ api/add-bot.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).json({ error: "Método não permitido" });
 
   try {
-    const { bot, userId } = req.body;a
+    const { bot, userId } = req.body;
 
     if (!bot || !userId) return res.status(400).json({ error: "Dados incompletos" });
 
